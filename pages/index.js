4677bcdbd3bb945e0bbbc041f0cabe23faca0cf2
evgenyvodyannikov <66135471+evgenyvodyannikov@users.noreply.github.com>
2022-09-19 23:39:27 +0500

    tailwind added, images from assets imported

--- pages/index.js
+++ pages/index.js
@@ -1,71 +1,42 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import Button from '@mui/material/Button';
 
-export default function Home(){
+export default function Home() {
     return (
-        
-    <body>
-        <header>
-            <img src="../assets/image/newLogo.png" alt="smth"></img>
-            <nav>
-                <ul>
-                    <li><a href="#">App</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Plan travel</a></li>
-                    <li><a href="#">About</a></li>
-                </ul>
-            </nav>
-        </header>
-    
-        <main>
-            <div class="info">
-                <img src="../assets/image/mountains.jpg" alt="123"></img>
-                <div>Find your adventure</div>
-            </div>
-            <section class="offers_page">
-                <div class="offers_list">
-                    <div class="offer">
-                      <img src="../assets/image/icons/mountain.png" alt="123"></img>
-                        <h3>WHY US?</h3>
-                        <p>We'he been creating roundtrips for 17 years and we're determined to provide you with the trip
-                        </p>
-                    </div>
-                    <div class="offer">
-                       <img src="../assets/image/icons/mountain.png" alt="123"></img>
-                        <h3>PLAN YOUR TRAVEL?</h3>
-                        <p>You're in charge! Create your own roundtrip with our extensive services in cars, hotels and
-                            flights. </p>
-                    </div>
-                    <div class="offer">
-                        <img src="../assets/image/icons/mountain.png" alt="123"></img> 
-                        <h3>HOLIDAY TOURS?</h3>
-                        <p>Book travel tours worldwide including adventure and cultural tours from leading providers such as
-                            Intrepid, Trafalgar, Contiki</p>
-                    </div>
-                </div>
-            </section>
-    
-            <section class="adventure_pg_01">
-                <div class="adventure_text">
-                    <h1>Where the adventure never ends</h1>
-                    <h2>Create your perfect trip</h2>
-                    <p>Explore the roads less travelled, where your passion for outdoor adventure can roam wild and your
-                        appreciation for nature deepens. Stretching from the spectacular fjords and lush islands of Kenai
-                        Fjords National Park to the herculean peak of Denali in Denali National Park, Alaska is a land of
-                        epic proportions. </p>
-                </div>
-                <div class="adventure_counter">
-                    <h1>01</h1>
-                </div>
-            </section>
+        <>
+            <div className="flex min-h-screen flex-col items-center justify-center py-2">
+                <Head>
+                    <title>Next App</title>
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
 
-        </main>
-    
-        <footer>
-            2022 &copy; RedExinus
-        </footer>
-    
-        <script src="./script.js"></script>
-    </body>
+                <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+                    <h1 className="text-6xl font-bold">
+                       Here is{' '}
+                        <a className="text-blue-600" href="#">
+                            Description{' '} 
+                        </a>
+                        below
+                    </h1>
+
+                    <p className="mt-3 text-2xl">
+                        Tailwind added, structure changed. Was made decision to start from zero.{' '}
+                    </p>
+
+                </main>
+
+                <footer className="flex h-24 w-full items-center justify-center border-t">
+                    <a
+                        className="flex items-center justify-center gap-2"
+                        href="#"
+                        rel="noopener noreferrer"
+                    >
+                        Powered by Me
+                    </a>
+                </footer>
+            </div>
+        </>
     )
-    
+
 }
